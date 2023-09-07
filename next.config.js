f5c24bebaa5154f98a6989e44fb85582ad8341e4
Misha Shaposhnikov <66800817+mishapos@users.noreply.github.com>
2023-09-07 08:36:33 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,42 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  output: 'standalone',
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'assets.vercel.com',
-        port: '',
-        pathname: '/image/upload/**',
-      },
-    ],
-  },
+// module.exports = {
+//   output: 'standalone',
+//   images: {
+//     formats: ['image/avif', 'image/webp'],
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'assets.vercel.com',
+//         port: '',
+//         pathname: '/image/upload/**',
+//       },
+//     ],
+//   },
+// }
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+
+const nextConfig = {
+    // environments
+    publicRuntimeConfig: {
+        COMPANY: '셀타스퀘어',
+    },
+
+    reactStrictMode: false,
+    swcMinify: true,
+    output: 'standalone',
+
+    async rewrites() {
+        return [
+            {
+                source: '/v2/api/:path*',
+                destination: 'http://localhost:8000/:path*',
+            },
+        ]
+    },
 }
+
+module.exports = withBundleAnalyzer(nextConfig)
+
